0b74d09fd393ff551e9baab64fc4468f2d58b61e
mike17sc <mike17sc@hotmail.com>
2017-05-23 18:52:59 +0200

    init

--- src/main/java/be/simac/homework/SortClient.java
+++ src/main/java/be/simac/homework/SortClient.java
@@ -1,22 +1,30 @@
 package be.simac.homework;
+
 import java.util.*;
 
 /**
  * Created by Mschneider on 23-05-17.
  */
 public class SortClient {
-    public static List<Client> sortClient(List<Client> listClient){
-        Collections.sort(listClient,new Comparator<Client>(){
-           @Override
-            public int compare(Client clientOne,Client clientTwo){
-               int compare= clientOne.name.compareTo(clientTwo.name);
-               if(compare==0){
-                   return clientOne.age - clientTwo.age;
-               }
-               else{
-                   return compare;
-               }
-           }
+    public static List<Client> sortClient(List<Client> listClient) throws MyException {
+        if (listClient == null) {
+            throw new MyException("List can't be null");
+        }
+        for (int i = 0; i < listClient.size(); i++) {
+            if (listClient.get(i) == null) {
+                throw new MyException("List can't contain null client");
+            }
+        }
+        Collections.sort(listClient, new Comparator<Client>() {
+            @Override
+            public int compare(Client clientOne, Client clientTwo) {
+                int compare = clientOne.name.compareTo(clientTwo.name);
+                if (compare == 0) {
+                    return clientOne.age - clientTwo.age;
+                } else {
+                    return compare;
+                }
+            }
         });
         return listClient;
     }
--- src/test/java/be/simac/homework/SortClientTest.java
+++ src/test/java/be/simac/homework/SortClientTest.java
@@ -1,6 +1,9 @@
 package be.simac.homework;
+
 import org.junit.Test;
+
 import static org.assertj.core.api.Assertions.*;
+
 import java.util.*;
 
 /**
@@ -9,14 +12,40 @@ import java.util.*;
 
 public class SortClientTest {
     @Test
-    public void sortClientTest(){
-        List<Client> list=new ArrayList<>();
-        list.add(new Client("Mike",19));
-        list.add(new Client("Annick",36));
-        list.add(new Client("Mike",6));
-        list=SortClient.sortClient(list);
-        for(int i=0;i<list.size();i++){
-            System.out.println(list.get(i).getName() + list.get(i).getAge());
+    public void sortClientTest() throws MyException {
+        List<Client> list = new ArrayList<>();
+        list.add(new Client("Mike", 19));
+        list.add(new Client("Annick", 36));
+        list.add(new Client("Mike", 6));
+        list = SortClient.sortClient(list);
+        assertThat(list.get(0).getName()).isEqualTo("Annick");
+        assertThat(list.get(1).getName()).isEqualTo("Mike");
+        assertThat(list.get(1).getAge()).isEqualTo(6);
+        assertThat(list.get(2).getName()).isEqualTo("Mike");
+        assertThat(list.get(2).getAge()).isEqualTo(19);
+    }
+    @Test
+    public void sortClientNullListTest() throws MyException {
+        List<Client> list = null;
+        try {
+            list = SortClient.sortClient(list);
+        } catch (MyException e){
+            assertThat(e).hasMessage("List can't be null");
         }
+    }
+    @Test
+    public void sortClientNullClientTest() throws MyException {
+        List<Client> list = new ArrayList<>();
+        list.add(null);
+        list.add(new Client("Annick", 36));
+        list.add(new Client("Mike", 6));
+        try{
+            list = SortClient.sortClient(list);
+        }
+        catch(MyException e){
+            assertThat(e).hasMessage("List can't contain null client");
+        }
+
+
     }
 }
