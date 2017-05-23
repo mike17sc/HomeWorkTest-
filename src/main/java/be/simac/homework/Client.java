d9095c46f1174dac8ed3858c8c11d6439c4ebf81
mike17sc <mike17sc@hotmail.com>
2017-05-23 17:33:14 +0200

    init

---
+++ src/main/java/be/simac/homework/Client.java
@@ -0,0 +1,29 @@
+package be.simac.homework;
+
+/**
+ * Created by Mschneider on 23-05-17.
+ */
+public class Client {
+    public  Client(String name,int age){
+        this.age=age;
+        this.name=name;
+    }
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
